48d93693378a64c2261142f658e0445b269e155c
Alexey Gorshkov <animus@wayround.org>
2016-07-11 03:35:53 +0300

    ic

--- Makefile
+++
@@ -1,9 +0,0 @@
-
-PYTHON=python3
-
-all:
-	$(PYTHON) ./setup.py build_ext --inplace
-
-clean:
-	make -C wayround_org/toxcorebind clean
-
--- make.sh
+++ make.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-reset ; make
+
+rm wayround_org/toxcorebind/*.so
+rm wayround_org/toxcorebind/*.c
+
+reset
+
+python3 ./setup.py build_ext --inplace
+
